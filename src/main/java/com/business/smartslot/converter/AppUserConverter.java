783265b00b7a15641853d022f45a831facf48e7d
sasi-kalyan <kanakamsasikalyan1@gmail.com>
2026-06-16 12:05:56 +0530

    created convert dto-entity vice-versa

---
+++ src/main/java/com/business/smartslot/converter/AppUserConverter.java
@@ -0,0 +1,48 @@
+package com.business.smartslot.converter;
+
+import com.business.smartslot.dto.AppUserDTO;
+import com.business.smartslot.entity.AppUser;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class AppUserConverter {
+
+    public AppUser convertDTOtoEntity(AppUserDTO appUserDto){
+
+        AppUser appUser = new AppUser();
+        appUser.setUserName(appUserDto.getUserName());
+        appUser.setEmail(appUserDto.getEmail());
+        appUser.setFullName(appUserDto.getFullName());
+        appUser.setPhoneNumber(appUserDto.getPhoneNumber());
+        appUser.setPassword(appUserDto.getPassword());
+        appUser.setCreatedAt(getFormattedTime());
+        appUser.setUpdatedAt(getFormattedTime());
+
+        return appUser;
+    }
+
+    public AppUserDTO convertEntitytoDTO(AppUser appUser){
+
+        AppUserDTO appUserDto = new AppUserDTO();
+        appUserDto.setId(appUser.getId());
+        appUserDto.setUserName(appUser.getUserName());
+        appUserDto.setEmail(appUser.getEmail());
+        appUserDto.setFullName(appUser.getFullName());
+        appUserDto.setPhoneNumber(appUser.getPhoneNumber());
+        appUserDto.setCreatedAt(appUser.getCreatedAt());
+        appUserDto.setUpdatedAt(appUser.getUpdatedAt());
+
+        return appUserDto;
+    }
+
+    public String getFormattedTime(){
+        LocalDateTime ldt = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String formattedDateTime = ldt.format(dtf);
+        return formattedDateTime;
+    }
+}
