9389a487b1dbe4880720f3b1f2a167136ce31f72
sasi-kalyan <kanakamsasikalyan1@gmail.com>
2026-06-16 16:36:38 +0530

    created business-repo

---
+++ src/main/java/com/business/smartslot/repository/BusinessRepository.java
@@ -0,0 +1,9 @@
+package com.business.smartslot.repository;
+
+import com.business.smartslot.entity.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusinessRepository extends JpaRepository<Business, Long> {
+}
