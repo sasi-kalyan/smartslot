e74bb8f5bce2b8860b841bdfbe5a30389313fff8
sasi-kalyan <kanakamsasikalyan1@gmail.com>
2026-06-16 12:26:14 +0530

    added new field -role

--- src/main/java/com/business/smartslot/dto/AppUserDTO.java
+++ src/main/java/com/business/smartslot/dto/AppUserDTO.java
@@ -1,5 +1,6 @@
 package com.business.smartslot.dto;
 
+import com.business.smartslot.entity.Role;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AppUserDTO {
     private Long id;
+    private Role role;
     private String userName;
     private String password;
     private String fullName;
